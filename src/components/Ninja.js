c04740d6f830406202b4f4ce7c8ce0121d5653b6
Dylan Hu <dylan@Dylans-iMac.local>
2020-11-27 20:51:52 -0500

    fixed dependencies

--- src/components/Ninja.js
+++ src/components/Ninja.js
@@ -1,29 +1,29 @@
 import React, { Component } from "react";
 
 class Ninja extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { count: 0 };
-        this.handleClick = this.handleClick.bind(this); //fucking work
-    }
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+    this.handleClick = this.handleClick.bind(this); //fucking work
+  }
 
-    handleClick = () => {
-        this.setState({ count: this.state.count + 1 });
-    };
+  handleClick = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
 
-    componentDidMount() {
-        console.log("loaded");
-    }
+  componentDidMount() {
+    console.log("loaded");
+  }
 
-    render() {
-        return (
-            <div>
-                <h2>This is my class MyClassComponent</h2>
-                <p>{this.state.count}</p>
-                <button onClick={this.handleClick}>Count up!</button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h2>This is my class MyClassComponent</h2>
+        <p>{this.state.count}</p>
+        <button onClick={this.handleClick}>Count up!</button>
+      </div>
+    );
+  }
 }
 
 export default Ninja;
